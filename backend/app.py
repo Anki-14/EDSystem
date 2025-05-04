bb6d989a6808219a3bdf0d610ebc3cce17312c05
ANKITA GURSALI <142939251+Anki-14@users.noreply.github.com>
2025-05-04 17:20:52 +0530

    Update app.py
    Signed-off-by: ANKITA GURSALI <142939251+Anki-14@users.noreply.github.com>

--- backend/app.py
+++ backend/app.py
@@ -678,4 +678,6 @@ def detect_emotion():
 
 # Run Server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or fallback to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
